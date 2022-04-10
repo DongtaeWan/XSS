0083c003a4b981ee257b4ba75c00efe99f345a20
twdong <twdong@iochord.com>
2022-04-11 00:15:33 +0900

    fix : commit add

---
+++ src/test/java/com/xss/xss/XssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xss.xss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XssApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
